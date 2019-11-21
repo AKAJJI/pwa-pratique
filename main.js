aa19eb9a58acfbfab5149bd0d1d12743e38e2f25
Elkajji Ayoub <ayoub-op@hotmail.com>
2019-11-21 14:09:46 +0100

    4. Gestion de cache -resultat

--- main.js
+++ main.js
@@ -22,3 +22,15 @@ if(navigator.serviceWorker){
         .register('sw.js')
         .catch(err =>console.error('service worker NON enregistré',err));
 }
+
+/*if(window.caches){
+   caches.open('veille-techno-1.0').then(cache =>{
+        cache.addAll([
+            'index.html',
+            'main.js',
+            'vendors/bootstrap4.min.css'
+        ]);
+    });
+    //caches.open('other-1.0');
+    //caches.keys().then(console.log);
+}*/
